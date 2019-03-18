d88c0ffb32e78e7261722f3878c5514009f723b2
Khadijasg <khadijasg@code.edu.az>
2019-03-18 14:25:19 +0400

    (Ajax) Load More

---
+++ RentACar/Controllers/AjaxController.cs
@@ -0,0 +1,52 @@
+﻿using RentACar.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class AjaxController : Controller
+    {
+
+        private readonly CarContext _context;
+        public AjaxController()
+        {
+            _context = new CarContext();
+
+        }
+
+        // GET: Ajax
+        public ActionResult GetCars(int id)
+        {
+            var model = _context.Cars.Where(c => c.Id == id).Select(c => new
+            {
+                c.Id,
+                c.CarBrand,
+                c.CarModel,
+                c.CarYear,
+                c.EngineVolume,
+                c.HorsePower,
+                c.MilageVolume,
+                c.TypeOfEngine,
+                c.Image
+            });
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetCarsHtml(int id)
+        {
+            var model = _context.Cars.Where(c => c.Id == id);
+
+            return PartialView("Load",model);
+        }
+
+        public ActionResult Load(int skip)
+        {
+            return PartialView(_context.Cars.OrderBy(c => c.Id).Skip(skip).Take(3));
+        }
+
+    }
+}
---
+++ RentACar/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentACar.DAL;
+using RentACar.Models;
+
+namespace RentACar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly CarContext _context;
+        public HomeController()
+        {
+            _context = new CarContext();
+
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.TotalCarsCount = _context.Cars.Count();
+            return View(_context.Cars.OrderBy(c => c.Id).Take(3));
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Car car)
+        {
+            if(!ModelState.IsValid)
+            {
+
+                return View(car);
+
+            }
+
+
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
